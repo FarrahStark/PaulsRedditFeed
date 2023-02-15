0b56a45df19c9565dda252cbc39e9cc2c282c7a9
Paul Stark <paulstegler@gmail.com>
2023-02-15 15:46:33 -0700

    adds logging and TwitterFeed Background Service

---
+++ PaulsTwitterFeed/Services/TwitterFeed.cs
@@ -0,0 +1,28 @@
+﻿namespace PaulsTwitterFeed
+{
+    public class TwitterFeed : BackgroundService
+    {
+        private readonly ILogger<TwitterFeed> logger;
+
+        public TwitterFeed(ILogger<TwitterFeed> logger)
+        {
+            this.logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            logger.LogInformation($"${nameof(TwitterFeed)} Background Service Started");
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await ReadStreamAsync();
+            }
+            logger.LogInformation($"${nameof(TwitterFeed)} Background Service Stopped");
+        }
+
+        private async Task ReadStreamAsync()
+        {
+            logger.LogInformation("Reading filtered twitter stream");
+            await Task.Delay(1000);
+        }
+    }
+}
