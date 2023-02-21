d330b2f156321fc52aa0d95364741369abdd169f
Paul Stark <paulstegler@gmail.com>
2023-02-21 01:56:55 -0700

    Subreddit info list rendering

---
+++ PaulsRedditFeed/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PaulsRedditFeed
+{
+    public class HomeController : Controller
+    {
+        [HttpPut("/subreddits")]
+        public async Task<IActionResult> Subreddits([FromBody] List<SubredditViewModel> model)
+        {
+            return PartialView("_SubredditList", model);
+        }
+    }
+}
---
+++ PaulsRedditFeed/Models/SubredditViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PaulsRedditFeed
+{
+    public class SubredditViewModel
+    {
+        public string Title { get; set; } = "";
+        public int ActiveUserCount { get; set; } = 0;
+        public string TopPostTitle { get; set; } = "";
+    }
+}
