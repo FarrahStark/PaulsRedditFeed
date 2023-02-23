f83d56bcd2723f1bbd1a2ad69465090e8c1bab24
Paul Stark <paulstegler@gmail.com>
2023-02-22 18:33:06 -0700

    Server ready to connect to redis cache

---
+++ PaulsRedditFeed/Exceptions/CachingException.cs
@@ -0,0 +1,9 @@
+﻿namespace PaulsRedditFeed
+{
+    public class CachingException : Exception
+    {
+        public CachingException(string message, Exception? innerException = null) : base(message, innerException)
+        {
+        }
+    }
+}
